a9788c3668123b3722f6fcb0627b36b7d3327469
yuguo_bimsa <719812710@qq.com>
2026-02-06 12:31:31 +0800

    重构幅度阻尼类以使用自定义Kraus算子，简化噪声设置逻辑

--- vgqec/environment/amplitudedamping.py
+++ vgqec/environment/amplitudedamping.py
@@ -2,6 +2,19 @@ from .envbase import EnvBase
 from qiskit_aer import noise as noise_aer
 import qiskit
 
+import numpy as np
+from itertools import product
+from functools import reduce
+
+def amp_damp_single_kraus(lam: float):
+    lam = float(lam)
+    K0 = np.array([[1.0, 0.0],
+                   [0.0, np.sqrt(1.0 - lam)]], dtype=complex)
+    K1 = np.array([[0.0, np.sqrt(lam)],
+                   [0.0, 0.0]], dtype=complex)
+    return K0, K1
+
+
 class AmplitudeDamping(EnvBase):
     def __init__(self,n,lam=0.1):
         super().__init__()
@@ -10,10 +23,18 @@ class AmplitudeDamping(EnvBase):
         self.set_noise()
 
     def set_noise(self):
-        circuit = qiskit.QuantumCircuit(self.n)
-        error = noise_aer.amplitude_damping_error(self.lam)
-        for i in range(self.n):
-            circuit.append(error, [i])
+        K0, K1 = amp_damp_single_kraus(self.lam)
+        ops = (K0, K1)
 
-        kraus_operators = qiskit.quantum_info.Kraus(circuit).data
-        self.noise_kraus=kraus_operators
+        def kron_qiskit_order(mats_for_q0_to_qn1):
+            # Qiskit order: leftmost factor is qubit n-1, rightmost is qubit 0
+            return reduce(np.kron, mats_for_q0_to_qn1[::-1])
+
+        iterable = product([0, 1], repeat=self.n)
+
+
+        out = []
+        for bits in iterable:
+            mats = [ops[b] for b in bits]
+            out.append(kron_qiskit_order(mats))
+        self.noise_kraus=out
