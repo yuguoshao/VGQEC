c321bf5301e8ca9fd4dfeed48a48ceb54714b246
yuguo_bimsa <719812710@qq.com>
2026-02-09 00:07:08 +0800

    优化幅度阻尼类，初始化时设置局部Kraus算子以提高噪声处理效率

--- vgqec/environment/amplitudedamping.py
+++ vgqec/environment/amplitudedamping.py
@@ -20,6 +20,7 @@ class AmplitudeDamping(EnvBase):
         super().__init__()
         self.n=n
         self.lam=lam
+        self._local_kraus=None
         self.set_noise()
 
     def set_noise(self):
@@ -32,9 +33,28 @@ class AmplitudeDamping(EnvBase):
 
         iterable = product([0, 1], repeat=self.n)
 
+        local_kraus = []
+        ident = np.eye(2, dtype=complex)
+        for q in range(self.n):
+            mats0 = [ident] * self.n
+            mats1 = [ident] * self.n
+            mats0[q] = K0
+            mats1[q] = K1
+            L0 = kron_qiskit_order(mats0)
+            L1 = kron_qiskit_order(mats1)
+            local_kraus.append((L0, L0.conj().T, L1, L1.conj().T))
+        self._local_kraus = local_kraus
 
         out = []
         for bits in iterable:
             mats = [ops[b] for b in bits]
             out.append(kron_qiskit_order(mats))
         self.noise_kraus=out
+
+    def evo_mat(self, density_matrix):
+        # Independent amplitude damping on n qubits can be applied qubit-by-qubit.
+        # This is equivalent to summing over 2^n global Kraus operators but much faster.
+        res = np.asarray(density_matrix, dtype=np.complex128)
+        for L0, L0_dag, L1, L1_dag in self._local_kraus:
+            res = L0 @ res @ L0_dag + L1 @ res @ L1_dag
+        return res
